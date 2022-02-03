1ba82f4556d2891f92d5a65b71679f3968b6bf22
Michel Drescher <michel.drescher@gmail.com>
2022-02-03 16:57:08 +0100

    Add intermediate lines for each segtor/segment

--- src/server/controllers/radar-render/renderer.js
+++ src/server/controllers/radar-render/renderer.js
@@ -152,21 +152,40 @@ const plotRing = (root, ringName, blips, segIdx, ringIdx, angles, radii, blipDia
 }
 
 const plotLines = (group, startA, endA, radius) => {
-    // lines always start at (0, 0)
-    // SVG coordinate system is mirrored on x axis
-    // hence we need to rotate by 90 degree, i.e.PI/2
-    //to get the correct coordinates
+    // 1.) "Left" and "Right" lines
+    // lines always start at (0, 0). 
+    // SVG coordinate system is mirrored on x axis hence we need to rotate by 90 degree, i.e. PI/2
+    // to get the correct coordinates
+    // "Left" line endpoints
+    const endLeftX = radius * Math.cos(startA - Math.PI / 2)
+    const endLeftY = radius * Math.sin(startA - Math.PI / 2)
+    // "Right" line endpoints
+    const endRightX = radius * Math.cos(endA - Math.PI / 2)
+    const endRightY = radius * Math.sin(endA - Math.PI / 2)
 
-    // line 1 endpoints
-    const endX1 = radius * Math.cos(startA - Math.PI / 2)
-    const endY1 = radius * Math.sin(startA - Math.PI / 2)
+    // 2.) Intermetiate lines
+    // We know we need to divide the segment at hand into three equal pies.
+    // We also have the start angle, and end angle, in RAD.
+    // The angle (in RAD) for each pie is: offs = (endA - startA) / 3 (three equal pies)
+    // hence the offset for the intermediate left separator line is: startA + offs - Math.PI / 2
+    // the offset for the intermediate right separator line is: endA - offs - Math.PI / 2
+    const offs = (endA - startA) / 3
+    // "Intermediate left" line end points 
+    const endIntLeftX = radius * Math.cos(startA + offs - Math.PI / 2)
+    const endIntLeftY = radius * Math.sin(startA + offs - Math.PI / 2)
+    // "Intermediate right" line end points 
+    const endIntRightX = radius * Math.cos(endA - offs - Math.PI / 2)
+    const endIntRightY = radius * Math.sin(endA - offs - Math.PI / 2)
 
-    // line 2 endpoints
-    const endX2 = radius * Math.cos(endA - Math.PI / 2)
-    const endY2 = radius * Math.sin(endA - Math.PI / 2)
 
     // drawing "left" line
-    group.append('line').attr('x1', 0).attr('y1', 0).attr('x2', endX1).attr('y2', endY1)
+    group.append('line').attr('x1', 0).attr('y1', 0).attr('x2', endLeftX).attr('y2', endLeftY)
+    // drawing "intermediate left" line
+    group.append('line').attr('x1', 0).attr('y1', 0).attr('x2', endIntLeftX).attr('y2', endIntLeftY)
+         .attr('class', 'sub')
+    // drawing "intermediate right" line
+    group.append('line').attr('x1', 0).attr('y1', 0).attr('x2', endIntRightX).attr('y2', endIntRightY)
+         .attr('class', 'sub')
     // draw "right" line
-    group.append('line').attr('x1', 0).attr('y1', 0).attr('x2', endX2).attr('y2', endY2)
+    group.append('line').attr('x1', 0).attr('y1', 0).attr('x2', endRightX).attr('y2', endRightY)
 }
